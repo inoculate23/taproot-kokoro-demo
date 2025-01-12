d00b2aff74b8fe84c3c1754a7ae4b4db4d797fdc
Benjamin Paine <painebenjamin@gmail.com>
2025-01-12 17:50:59 -0500

    update display, add more comments

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
-/** @module index */
-
 // Create Taproot client
-let overseerAddress;
-if (window.location.port === "3000") {
-    // Development (e.g. npm start)
-    overseerAddress = "ws://localhost:32189";
-} else {
-    // Docker or production
-    overseerAddress = "overseer";
-}
-const client = new Taproot(overseerAddress);
+const client = new Taproot("ws://127.0.0.1:32189");
 
 // Get page elements
 const voiceSelect = document.querySelector("select#voice");
@@ -19,10 +9,12 @@ const audioContainer = document.querySelector("#audio");
 const latency = document.querySelector("#latency");
 
 // Only keep one audio object so we don't overload memory
-let audioObject = null,
-    generating = false;
+let audioObject = null;
+// We use a global flag to help with enabling/disabling the submit button
+let generating = false;
 
-// Bind text input event listener
+// Bind text input event listener to enable/disable submit
+// button when text input is empty/non-empty and not generating
 textInput.addEventListener("input", (event) => {
     if (generating) { return; }
     if (textInput.value.length > 0) {
@@ -31,45 +23,62 @@ textInput.addEventListener("input", (event) => {
         submitButton.disabled = true;
     }
 });
+
+// Bind text keydown event listener to submit on Ctrl+Enter
 textInput.addEventListener("keydown", (event) => {
-    if (event.key === "Enter" && event.ctrlKey) {
+    if (event.key === "Enter" && event.ctrlKey && !generating) {
         submitButton.click();
         event.preventDefault();
         event.stopPropagation();
     }
 });
-// Bind submit event listener
+
+// Bind submit button event listener to generate audio
 submitButton.addEventListener("click", async () => {
+    // Disable submit button
     generating = true;
     submitButton.disabled = true;
+
+    // Keep track of start time for latency calculation
     const startTime = Date.now();
+
+    // Generate audio
     const audio = await client.invoke({
         task: "speech-synthesis",
         model: "kokoro",
         parameters: {
             text: textInput.value,
             voice: voiceSelect.value,
-            enhance: true,
+            enhance: true, // Upsample 24kHz to 48kHz with DeepFilterNet
             output_format: "wav"
         }
     });
+
+    // Calculate real-time factor and update latency display
     const executionMilliseconds = Date.now() - startTime;
     const audioMilliseconds = audio.duration * 1000;
     const realTimeFactor = audioMilliseconds / executionMilliseconds;
+    latency.innerText = `Generated ${audio.duration.toFixed(1)}s audio in ${executionMilliseconds}ms (${realTimeFactor.toFixed(1)}x real-time)`;
 
-    generating = false;
-    if (textInput.value.length > 0) {
-        submitButton.disabled = false;
-    }
+    // Enable audio controls and play audio
+    audio.controls = true;
+    audio.play();
 
+    // Add audio to page
+    audioContainer.innerHTML = "";
+    audioContainer.appendChild(audio);
+
+    // Revoke previous audio object URL to free memory
     if (audioObject !== null) {
         URL.revokeObjectURL(audioObject.src);
     }
 
-    audioContainer.innerHTML = "";
-    audioContainer.appendChild(audio);
-    audio.controls = true;
-    audio.play();
-    latency.innerText = `Generated ${audio.duration.toFixed(1)}s audio in ${executionMilliseconds}ms (${realTimeFactor.toFixed(1)}x real-time)`;
+    // Keep track of current audio object
     audioObject = audio;
+
+    // Re-enable submit button
+    generating = false;
+    if (textInput.value.length > 0) {
+        submitButton.disabled = false;
+    }
 });
